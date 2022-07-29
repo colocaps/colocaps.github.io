c5e685661792a2fabcc898339bb36782205f5aaa
colocaps <56741608+colocaps@users.noreply.github.com>
2022-07-28 23:48:31 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "assets/AssetManifest.json": "f3087212073b8ba6e024a777c38dfc67",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "8bbb19982998ae0cdd6cb2affd033095",
+"assets/NOTICES": "78b00f7cca807186b5aa73e68235a676",
 "assets/packages/core/assets/images/avatar_default.png": "6d7a8d509be59100ec749b9ea8ab81e7",
 "assets/packages/core/assets/images/coffee_paperclips_pencil_angled_bw_w1080.jpg": "9f499cf53bb008afe9f6bd89deba9c52",
 "assets/packages/core/assets/images/iphone_cactus_tea_overhead_bw_w1080.jpg": "319249e756f3a470d45841baea9d1132",
@@ -25,9 +25,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3a6d9a5251bb34241920c05d39976717",
-"/": "3a6d9a5251bb34241920c05d39976717",
-"main.dart.js": "a76df5784996d51cc6c3adfe71caa369",
+"index.html": "854960c0ca432557782626ef3390e30a",
+"/": "854960c0ca432557782626ef3390e30a",
+"main.dart.js": "f2d4f598ef883a9e9c939c596ae70e11",
 "manifest.json": "72502d9d3da5b36a182b4b750e595077",
 "version.json": "5d2cb7246cc278fd54a18e0d181fc762"
 };
